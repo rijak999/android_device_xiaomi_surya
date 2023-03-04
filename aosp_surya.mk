7072e6f96e25247ab8c1c60865f16bf54b65109c
rijak999 <rizakkamal93@gmail.com>
2023-03-04 13:02:41 +0000

    surya: I'ts BiancaProject

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit Google Apps
+WITH_GMS := true
 
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
