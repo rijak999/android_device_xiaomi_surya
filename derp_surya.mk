0205f3448e429343088ba033c45a386db26cbea7
rijak999 <rizakkamal93@gmail.com>
2023-03-15 00:11:26 +0000

    surya: It's DerpFest

--- derp_surya.mk
+++ derp_surya.mk
@@ -11,10 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_surya
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Derp stuff
+DERP_BUILDTYPE := Official
+
+PRODUCT_NAME := derp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
